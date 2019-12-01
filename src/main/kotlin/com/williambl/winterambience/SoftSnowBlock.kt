12901718fd7c47fee9ebab2d6b673b8a17a2aca7
williambl <william@williambl.com>
2019-12-01 20:18:33 +0000

    Make soft snow slowly melt down when in the sunlight

--- src/main/kotlin/com/williambl/winterambience/SoftSnowBlock.kt
+++ src/main/kotlin/com/williambl/winterambience/SoftSnowBlock.kt
@@ -7,7 +7,9 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.shapes.ISelectionContext
 import net.minecraft.util.math.shapes.VoxelShape
 import net.minecraft.world.IBlockReader
+import net.minecraft.world.LightType
 import net.minecraft.world.World
+import java.util.*
 
 class SoftSnowBlock(properties: Properties) : SnowBlock(properties) {
 
@@ -19,5 +21,13 @@ class SoftSnowBlock(properties: Properties) : SnowBlock(properties) {
         if (state.get(LAYERS) > 4)
             entity.motion = entity.motion.mul(0.6, 1.0, 0.6)
         super.onEntityCollision(state, world, pos, entity)
+    }
+
+    override fun tick(state: BlockState, world: World, pos: BlockPos, rand: Random) {
+        if (world.getLightFor(LightType.SKY, pos) == 15 && !world.isRaining) {
+            if (state.get(LAYERS) > 1)
+                world.setBlockState(pos, state.with(LAYERS, state.get(LAYERS) - 1))
+        }
+        super.tick(state, world, pos, rand)
     }
 }
